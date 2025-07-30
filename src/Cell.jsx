d8f16860ce8ddd100add92d95851ac84c51d86a0
aeliyadevs <aeliyadevs@gmail.com>
2025-07-30 23:22:40 +0545

    make the cell respond to only the first click

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 
 function Cell(props) {
   const [image, setImage] = useState("");
+  // const [isPlayed, setIsPlayed] = useState(false);
+  const [notPlayed, setNotPlayed] = useState(true);
   let x = 10;
   console.log(props.activePlayer);
+
   function setImageSymbol() {
-    console.log(props.changeActivePlayer);
-    if (props.activePlayer == 1) {
-      setImage("./circle.svg");
-      props.changeActivePlayer(2);
-    } else {
-      setImage("./x.svg");
-      props.changeActivePlayer(1);
+    if (notPlayed) {
+      if (props.activePlayer == 1) {
+        setImage("./circle.svg");
+        props.changeActivePlayer(2);
+      } else {
+        setImage("./x.svg");
+        props.changeActivePlayer(1);
+      }
+      setNotPlayed(false);
     }
   }
   return (
